fe7e5e878e6b63183e0ff204dfe43711d64c919d
SammieMenz <sammieackun@hotmail.com>
2026-01-19 19:51:06 +0000

    fileupload getbyid amended

--- assetmanagement.api/DAL/Services/AwsService/IS3Service.cs
+++ assetmanagement.api/DAL/Services/AwsService/IS3Service.cs
@@ -6,7 +6,7 @@ public interface IS3Service
 {
     Task<ListObjectsV2Response> GetAllAsync();
     Task<string> CreateAsync(IFormFile file, bool isLogo);
-    Task<GetObjectResponse> GetByIdAsync(string key);
+    Task<GetObjectResponse> GetByIdAsync(string key, bool isLogo);
     Task<string> UpdateAsync(string key, IFormFile file, bool isLogo);
     Task DeleteAsync(string key, bool isLogo);
 }
